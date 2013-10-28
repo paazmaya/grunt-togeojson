45468013050a86fe80a805be0ff8a7078e48ad20
Juga Paazmaya <paazmaya@yahoo.com>
2013-10-28 14:25:02 +0200

    matchdep, learned from https://speakerdeck.com/jackfranklin/giving-your-development-more-grunt-fowa-london-2013

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,10 +62,8 @@ module.exports = function(grunt) {
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  // These plugins provide necessary tasks.  
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
   
   // Code coverage
   grunt.loadNpmTasks('grunt-karma');
